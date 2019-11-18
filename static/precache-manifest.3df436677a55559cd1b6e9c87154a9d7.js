2ed4fc96e473ea85d49f6115cd9ab4d02da8b817
Jan Kontek <janekk8000@gmail.com>
2019-11-18 05:38:47 +0100

    Test GitHubPages + React
    1
    2
    3

---
+++ static/precache-manifest.3df436677a55559cd1b6e9c87154a9d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3420a295e9783f358ac4c24399fb21a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cea3acfb1a4497a4ea05",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f4d956ae4694b54682e6",
+    "url": "/static/js/2.f1f464f2.chunk.js"
+  },
+  {
+    "revision": "cea3acfb1a4497a4ea05",
+    "url": "/static/js/main.dc4d6404.chunk.js"
+  },
+  {
+    "revision": "031e86a86113a4d78fd6",
+    "url": "/static/js/runtime-main.9c3dc9a1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
